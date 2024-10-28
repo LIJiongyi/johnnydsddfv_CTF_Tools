25c94cd64ac3a60f0549c53ad1e4402d0c6474cd
ljy <1924148097@qq.com>
2024-10-28 20:59:49 +0800

    Update base64_decode_encode.py

--- misc&crypto&forensics/base64_decode_encode.py
+++ misc&crypto&forensics/base64_decode_encode.py
@@ -5,7 +5,7 @@
 import base64
 
 # 要编码的原始数据
-data = "Hello, World!"
+data ="cGljb0NURnt3ZWJfc3VjYzNzc2Z1bGx5X2QzYzBkZWRfMTBmOTM3NmZ9"
 # 将字符串编码为字节
 byte_data = data.encode('utf-8')
 
@@ -15,7 +15,7 @@ encoded_data = base64.b64encode(byte_data)
 # 打印编码后的数据
 print("Encoded:", encoded_data.decode('utf-8'))
 # 进行Base64解码
-decoded_data = base64.b64decode(encoded_data)
+decoded_data = base64.b64decode(byte_data)
 
 # 打印解码后的数据
 print("Decoded:", decoded_data.decode('utf-8'))
